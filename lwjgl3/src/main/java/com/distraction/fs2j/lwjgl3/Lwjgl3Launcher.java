046aac8fdcb9b4fbf6caf1462d9a0fda64a678fd
Mike <foreign.guy.mike@gmail.com>
2022-04-12 10:21:34 -0400

    added individual sprites and paint.net file

--- lwjgl3/src/main/java/com/distraction/fs2j/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/distraction/fs2j/lwjgl3/Lwjgl3Launcher.java
@@ -1,21 +1,25 @@
 package com.distraction.fs2j.lwjgl3;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.distraction.fs2j.Constants;
 import com.distraction.fs2j.FlippySlime2J;
 
-/** Launches the desktop (LWJGL3) application. */
+/**
+ * Launches the desktop (LWJGL3) application.
+ */
 public class Lwjgl3Launcher {
-	public static void main(String[] args) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle(Constants.TITLE);
-		config.useVsync(true);
-		config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
-//		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
-		config.setWindowedMode(Constants.DESKTOP_WIDTH, Constants.DESKTOP_HEIGHT);
-		config.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
-		new Lwjgl3Application(new FlippySlime2J(), config);
-	}
+    public static void main(String[] args) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle(Constants.TITLE);
+        config.useVsync(true);
+        config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        if (Constants.FULLSCREEN) {
+            config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        } else {
+            config.setWindowedMode(Constants.DESKTOP_WIDTH, Constants.DESKTOP_HEIGHT);
+        }
+        config.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        new Lwjgl3Application(new FlippySlime2J(), config);
+    }
 }
