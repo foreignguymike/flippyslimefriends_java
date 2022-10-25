f43993a8731bea8213a43a57806cb266e4d89730
Mike <foreign.guy.mike@gmail.com>
2022-10-25 16:20:28 -0400

    versioning 1.0.1

--- core/src/main/java/com/distraction/fs2j/Constants.java
+++ core/src/main/java/com/distraction/fs2j/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final int DESKTOP_WIDTH = 480 * SCALE;
     public static final int DESKTOP_HEIGHT = 270 * SCALE;
 
-    public static final String VERSION = "v1.0.0";
+    public static final String VERSION = "v1.0.1";
 
     public static final boolean FULLSCREEN = false;
 
